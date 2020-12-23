fb508939bf52ce6fda6a38a8b616d5d475f459fc
bellerbrock <bje24@cornell.edu>
2020-12-23 14:36:40 -0500

    simplify printing

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Copy the Google Sheets Spreadsheet ID here
 # It's the value between the "/d/" and the "/edit" in the URL of your spreadsheet
 # ex. https://docs.google.com/spreadsheets/d/SPREADSHEET_ID/edit#gid=0
-SPREADSHEET_ID = ''
+SPREADSHEET_ID = '1BMW1JmsHRVQTZn7ggFSDVjr4XCiD5GLGWmTcXA6szXE'
 
 # Copy the name of the specific sheet within your spreadsheet you want to log to here
-SHEET_NAME = 'Sheet1'
+SHEET_NAME = 'Deck'
