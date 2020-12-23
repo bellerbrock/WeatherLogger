2314ac10d39d9ef69400b0af032424a16c28d0d1
bellerbrock <bje24@cornell.edu>
2020-12-23 14:02:56 -0500

    update script name and remove unneeded file

---
+++ config.py
@@ -0,0 +1,8 @@
+
+# Copy the Google Sheets Spreadsheet ID here
+# It's the value between the "/d/" and the "/edit" in the URL of your spreadsheet
+# ex. https://docs.google.com/spreadsheets/d/SPREADSHEET_ID/edit#gid=0
+SPREADSHEET_ID = ''
+
+# Copy the name of the specific sheet within your spreadsheet you want to log to here
+SHEET_NAME = 'Sheet1'
