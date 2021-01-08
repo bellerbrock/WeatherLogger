f1777ebe62a3834b5fff011677b0279caf52ec15
bellerbrock <bje24@cornell.edu>
2021-01-08 00:33:03 -0500

    update demo to include light intensity

--- demo.py
+++ demo.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import bme280
+import bh1750
 
 
 def main():
     bme = bme280.Bme280()
     bme.set_mode(bme280.MODE_FORCED)
-    t, p, h = bme.get_data()
-    print("Temperature: %f °C" % t)
-    print("Pressure: %f P" % p)
-    print("Humidity: %f %%" % h)
+    tempC, pressure, humidity = bme.get_data()
+    light = round(bh1750.readLight())
+    tempF = round((tempC * 9/5) + 32)
+    pressure = round(pressure/100)
+    humidity = round(humidity)
+    print ("Temperature:", tempF, "°F")
+    print ("Pressure:", pressure, "mb")
+    print ("Humidity:", humidity, "%%rH")
+    print ("Light Intensity:", light, "lux")
 
 
 if __name__ == '__main__':
